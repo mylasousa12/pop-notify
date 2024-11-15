59bef78a10eaf3b618faf241e101a5bdbd9af8dd
mylasousa12 <mila.sousa@hotmail.com>
2024-11-14 23:00:11 -0300

    Change size of the text and padding.

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ import Notification from "./src/js/Notification.js";
 
 window.onload = () => {
     document.getElementById('alert_button').addEventListener('click', function () {
-        Notification.success('Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 700000).fire();
-    });
+        Notification.info('Lorem Ipsum is simply dummy text of the printing and typesetting industry', 'aaaaaaaaaaaaaaaaaa',700000).fire()});
 }
